de58cd3a2f723330c76757ceb903e8ea22e18de8
Marcus Vinicius Estevam Candido <marcus.v.e.candido@gmail.com>
2022-07-01 18:10:40 -0300

    Finished project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entre com a largura e altura do retângulo:");
+		double width = sc.nextDouble(); 
+		double heigth = sc.nextDouble();
+		
+		Rectangle rectangle = new Rectangle(width, heigth);
+		
+		System.out.printf("AREA = %.2f %n", rectangle.area());
+		System.out.printf("PERIMETRO = %.2f %n", rectangle.perimeter());
+		System.out.printf("DIAGONAL = %.2f %n", rectangle.diagonal());
+		
+		
 
+		sc.close();
 	}
 
 }
